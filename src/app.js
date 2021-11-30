666c4be9927e002ba212a91865e35e2375bdb852
Amol Pradhan <amol.pradhan@nitorinfotech.net>
2021-11-30 15:36:27 +0530

    modifed setting for deployment

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const forecast = require('./utils/forecast');
 const geocode = require('./utils/geocode')
 const app = express();
 
+const port = process.env.PORT || 3000;
 // Define path for express config
 const publicDirPath = (path.join(__dirname,'../public'));
 const viewPath = path.join(__dirname,'../templates/views')
@@ -77,6 +78,6 @@ app.get('*',(req,res)=>{
     })
 
 })
-app.listen(3000,()=>{
-    console.log('Server is started on port: 3000');
+app.listen(port,()=>{
+    console.log('Server is started on port: '+port);
 })
